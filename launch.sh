c2d2e7e57cad3cbc7f2ebe98810daabeea3490cc
Nandini Sridharan <nandini9590@gmail.com>
2015-10-03 18:07:26 -0500

    Adding launch-script

--- launch.sh
+++ launch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash 
-
+#creating instances with parameters
 aws ec2 run-instances --image-id $1 --count $2 --instance-type $3 --key-name $4 --security-group-ids $5 --subnet-id $6 --associate-public-ip-address
